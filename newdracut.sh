126bbed63247867ac820f684f052d27729b4e74f
Harald Hoyer <harald@redhat.com>
2018-02-19 10:09:10 +0100

    version 047

---
+++ newdracut.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -e "$HOME/git/dracut/$1" ]]; then
+    srcrpm="$HOME/git/dracut/$1"
+else
+    srcrpm="$1"
+fi
+
+[[ -f $srcrpm ]] || exit 0
+
+cp dracut.spec dracut.spec.old
+for i in *.patch; do git rm -f $i;done
+
+if rpm -ivh --define "_srcrpmdir $PWD" --define "_specdir $PWD" --define "_sourcedir $PWD" "$srcrpm"; then
+	ls *.patch &>/dev/null && git add *.patch
+	perl -n -e 'if ($do_print) {print "$_" ;}; if (/^%changelog/) { $do_print=1; }' < dracut.spec.old >> dracut.spec
+fi
